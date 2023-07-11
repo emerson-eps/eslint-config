9d28b369339006bec0d5fbb04041b7701071fc82
Benjamin SÉGAULT <8903454+bsegault@users.noreply.github.com>
2023-07-11 10:16:39 +0200

    fix: add back rules for import/first and import/order (#118)
    BREAKING CHANGE: import first added back, so breaking from previous version that disabled it implicitely
    BREAKING CHANGE: added import ordering
    
    Co-authored-by: Benjamin SÉGAULT <Benjamin Ségault 8903454+bsegault@users.noreply.github.com>

--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,16 @@
 // A dummy files to test manually rules
+/* eslint-disable no-console */
 
-// eslint-disable-next-line no-console
+import _json5 from "json5";
+
+const first = 0;
+
+// eslint-disable-next-line import/first
+import pkg from "./package.json";
+
+// eslint-disable-next-line import/first, import/order
+import fs from "fs";
+
 console.log("hello");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,8 +19,9 @@ const empty = 1;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let dummy: any;
 
+console.log(fs.readSync);
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const getDummy = (param: unknown) => {
-  // eslint-disable-next-line no-console
-  console.log(`${dummy} and ${param}`);
+  console.log(`${dummy} and ${param} a, ${first}, ${pkg.version}`);
 };
